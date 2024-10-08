ee5030ebda6035bc90b9abec6e36e606021045ca
HoangKhoi1005 <158343641+HoangKhoi1005@users.noreply.github.com>
2024-10-08 10:24:24 +0700

    Create frmThemPhong(Create Phong), update Search

--- DAL/PhongDAL.cs
+++ DAL/PhongDAL.cs
@@ -122,5 +122,108 @@ namespace DAL
 
             return danhSachPhong;
         }
+
+        //Thêm phòng theo nhà trọ
+        public bool ThemPhong(PhongDTO phong)
+        {
+            string sql = "INSERT INTO PHONGTRO VALUES('" + phong.MaPT + "', '" + phong.MaNT + "', '" + phong.MaTT + "', N'" + phong.TenPhong + "', " + phong.DonGia + ", " + phong.ChieuDai + ", " + phong.ChieuRong + ", " + phong.SoLuongNguoiTD + ", N'" + phong.MoTa + "', '" + phong.Anh + "', " + phong.ThuTu + ")";
+
+            int kq = conn.ExecuteNonQuery(sql);
+
+            return kq > 0;
+        }
+
+        //Phát sinh mã phòng NT001-P01 tăng dần
+        public string PhatSinhMaPhong(string maNT)
+        {
+            string sql = "SELECT TOP 1 MAPT FROM PHONGTRO WHERE MANT = '" + maNT + "' ORDER BY MAPT DESC";
+
+            SqlDataReader reader = conn.ExecuteQuery(sql);
+
+            string maPhong = "";
+
+            if (reader.Read())
+            {
+                maPhong = reader["MAPT"].ToString();
+            }
+
+            reader.Close();
+            conn.close();
+
+            if (maPhong == "")
+            {
+                return maNT + "-P01";
+            }
+
+            string soPhong = maPhong.Substring(maPhong.Length - 2);
+            int so = Convert.ToInt32(soPhong) + 1;
+
+            if (so < 10)
+            {
+                return maNT + "-P0" + so;
+            }
+
+            return maNT + "-P" + so;
+        }
+
+        //Phát sinh thứ tự phòng
+        public int PhatSinhThuTuPhong(string maNT)
+        {
+            string sql = "SELECT TOP 1 THUTU FROM PHONGTRO WHERE MANT = '" + maNT + "' ORDER BY THUTU DESC";
+
+            SqlDataReader reader = conn.ExecuteQuery(sql);
+
+            int thuTu = 0;
+
+            if (reader.Read())
+            {
+                thuTu = Convert.ToInt32(reader["THUTU"]);
+            }
+
+            reader.Close();
+            conn.close();
+
+            return thuTu + 1;
+        }
+
+        public List<PhongDTO> TimKiemPhongTheoMaPhong(string maPhong, string maNT)
+        {
+            List<PhongDTO> danhSachPhong = new List<PhongDTO>();
+
+            try
+            {
+                string sql = "SELECT * FROM PHONGTRO WHERE MAPT LIKE '%" + maPhong + "%' AND MANT = '" + maNT + "'";
+
+                SqlDataReader reader = conn.ExecuteQuery(sql);
+
+                while (reader.Read())
+                {
+                    PhongDTO phong = new PhongDTO();
+
+                    phong.MaPT = reader["MAPT"].ToString();
+                    phong.MaNT = reader["MANT"].ToString();
+                    phong.MaTT = reader["MATT"].ToString();
+                    phong.TenPhong = reader["TENPHONG"].ToString();
+                    phong.DonGia = Convert.ToDecimal(reader["DONGIA"]);
+                    phong.ChieuDai = Convert.ToDouble(reader["CHIEUDAI"]);
+                    phong.ChieuRong = Convert.ToDouble(reader["CHIEURONG"]);
+                    phong.SoLuongNguoiTD = Convert.ToInt32(reader["SOLUONGNGUOITD"]);
+                    phong.MoTa = reader["MOTA"].ToString();
+                    phong.Anh = reader["ANH"].ToString();
+                    phong.ThuTu = Convert.ToInt32(reader["THUTU"]);
+
+                    danhSachPhong.Add(phong);
+                }
+
+                reader.Close();
+                conn.close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Lỗi khi tìm kiếm phòng theo mã phòng: " + ex.Message);
+            }
+
+            return danhSachPhong;
+        }
     }
 }
---
+++ GUI/frmThemPhong.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace GUI
+{
+    partial class frmThemPhong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTenPT = new System.Windows.Forms.TextBox();
+            this.txtMaPT = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnThem = new FontAwesome.Sharp.IconButton();
+            this.btnCancel = new FontAwesome.Sharp.IconButton();
+            this.cboTT = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDonGia = new System.Windows.Forms.TextBox();
+            this.txtChieuRong = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtChieuDai = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtMoTa = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtSoLuong = new Guna.UI2.WinForms.Guna2NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cboNT = new System.Windows.Forms.ComboBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.ptbAnh = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.txtSoLuong)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbAnh)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(278, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(257, 38);
+            this.label2.TabIndex = 38;
+            this.label2.Text = "THÊM PHÒNG TRỌ";
+            // 
+            // txtTenPT
+            // 
+            this.txtTenPT.Font = new System.Drawing.Font("Segoe UI", 10.2F);
+            this.txtTenPT.Location = new System.Drawing.Point(554, 161);
+            this.txtTenPT.Name = "txtTenPT";
+            this.txtTenPT.Size = new System.Drawing.Size(209, 30);
+            this.txtTenPT.TabIndex = 85;
+            // 
+            // txtMaPT
+            // 
+            this.txtMaPT.Font = new System.Drawing.Font("Segoe UI", 10.2F);
+            this.txtMaPT.Location = new System.Drawing.Point(195, 102);
+            this.txtMaPT.Multiline = true;
+            this.txtMaPT.Name = "txtMaPT";
+            this.txtMaPT.ReadOnly = true;
+            this.txtMaPT.Size = new System.Drawing.Size(181, 30);
+            this.txtMaPT.TabIndex = 84;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10.2F);
+            this.label3.Location = new System.Drawing.Point(422, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 23);
+            this.label3.TabIndex = 82;
+            this.label3.Text = "Tên Phòng Trọ:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.2F);
+            this.label1.Location = new System.Drawing.Point(45, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 23);
+            this.label1.TabIndex = 81;
+            this.label1.Text = "Mã Phòng Trọ:";
+            // 
+            // btnThem
+            // 
+            this.btnThem.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnThem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(175)))), ((int)(((byte)(80)))));
+            this.btnThem.FlatAppearance.BorderSize = 0;
+            this.btnThem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnThem.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThem.ForeColor = System.Drawing.Color.White;
+            this.btnThem.IconChar = FontAwesome.Sharp.IconChar.Plus;
+            this.btnThem.IconColor = System.Drawing.Color.White;
+            this.btnThem.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnThem.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnThem.Location = new System.Drawing.Point(155, 763);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnThem.Size = new System.Drawing.Size(206, 60);
+            this.btnThem.TabIndex = 88;
+            this.btnThem.Tag = "Xóa";
+            this.btnThem.Text = "Thêm Phòng";
+            this.btnThem.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnThem.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnThem.UseVisualStyleBackColor = false;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(67)))), ((int)(((byte)(54)))));
+            this.btnCancel.FlatAppearance.BorderSize = 0;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.ForeColor = System.Drawing.Color.White;
+            this.btnCancel.IconChar = FontAwesome.Sharp.IconChar.Ban;
+            this.btnCancel.IconColor = System.Drawing.Color.White;
+            this.btnCancel.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnCancel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCancel.Location = new System.Drawing.Point(426, 763);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnCancel.Size = new System.Drawing.Size(135, 60);
+            this.btnCancel.TabIndex = 87;
+            this.btnCancel.Tag = "Thêm";
+            this.btnCancel.Text = "Hủy";
+            this.btnCancel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCancel.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // cboTT
+            // 
+            this.cboTT.Font = new System.Drawing.Font("Segoe UI", 10.2F);
+            this.cboTT.FormattingEnabled = true;
+            this.cboTT.Location = new System.Drawing.Point(195, 167);
+            this.cboTT.Name = "cboTT";
+            this.cboTT.Size = new System.Drawing.Size(181, 31);
+            this.cboTT.TabIndex = 89;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 10.2F);
+            this.label5.Location = new System.Drawing.Point(45, 165);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 23);
+            this.label5.TabIndex = 91;
+            this.label5.Text = "Trạng Thái:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10.2F);
+            this.label4.Location = new System.Drawing.Point(422, 297);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 23);
+            this.label4.TabIndex = 92;
+            this.label4.Text = "Đơn Giá:";
+            // 
+            // txtDonGia
+            // 
+            this.txtDonGia.Font = new System.Drawing.Font("Segoe UI", 10.2F);
+            this.txtDonGia.Location = new System.Drawing.Point(554, 291);
+            this.txtDonGia.Name = "txtDonGia";
+            this.txtDonGia.Size = new System.Drawing.Size(209, 30);
+            this.txtDonGia.TabIndex = 93;
+            // 
+            // txtChieuRong
+            // 
+            this.txtChieuRong.Font = new System.Drawing.Font("Segoe UI", 10.2F);
+            this.txtChieuRong.Location = new System.Drawing.Point(554, 228);
+            this.txtChieuRong.Name = "txtChieuRong";
+            this.txtChieuRong.Size = new System.Drawing.Size(209, 30);
+            this.txtChieuRong.TabIndex = 95;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 10.2F);
+            this.label7.Location = new System.Drawing.Point(422, 230);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(103, 23);
+            this.label7.TabIndex = 94;
+            this.label7.Text = "Chiều Rộng:";
+            // 
+            // txtChieuDai
+            // 
+            this.txtChieuDai.Font = new System.Drawing.Font("Segoe UI", 10.2F);
+            this.txtChieuDai.Location = new System.Drawing.Point(195, 229);
+            this.txtChieuDai.Name = "txtChieuDai";
+            this.txtChieuDai.Size = new System.Drawing.Size(181, 30);
+            this.txtChieuDai.TabIndex = 97;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 10.2F);
+            this.label8.Location = new System.Drawing.Point(45, 228);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(88, 23);
+            this.label8.TabIndex = 96;
+            this.label8.Text = "Chiều Dài:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 10.2F);
+            this.label9.Location = new System.Drawing.Point(45, 298);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(138, 23);
+            this.label9.TabIndex = 98;
+            this.label9.Text = "Số Lượng Người:";
+            // 
+            // txtMoTa
+            // 
+            this.txtMoTa.Font = new System.Drawing.Font("Segoe UI", 10.2F);
+            this.txtMoTa.Location = new System.Drawing.Point(195, 359);
+            this.txtMoTa.Multiline = true;
+            this.txtMoTa.Name = "txtMoTa";
+            this.txtMoTa.Size = new System.Drawing.Size(554, 86);
+            this.txtMoTa.TabIndex = 101;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 10.2F);
+            this.label10.Location = new System.Drawing.Point(45, 359);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(62, 23);
+            this.label10.TabIndex = 100;
+            this.label10.Text = "Mô Tả:";
+            // 
+            // txtSoLuong
+            // 
+            this.txtSoLuong.BackColor = System.Drawing.Color.Transparent;
+            this.txtSoLuong.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtSoLuong.Font = new System.Drawing.Font("Segoe UI", 10.2F);
+            this.txtSoLuong.Location = new System.Drawing.Point(200, 291);
+            this.txtSoLuong.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtSoLuong.Name = "txtSoLuong";
+            this.txtSoLuong.Size = new System.Drawing.Size(176, 31);
+            this.txtSoLuong.TabIndex = 102;
+            this.txtSoLuong.UpDownButtonFillColor = System.Drawing.Color.Silver;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 10.2F);
+            this.label6.Location = new System.Drawing.Point(422, 104);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 23);
+            this.label6.TabIndex = 104;
+            this.label6.Text = "Nhà Trọ:";
+            // 
+            // cboNT
+            // 
+            this.cboNT.Font = new System.Drawing.Font("Segoe UI", 10.2F);
+            this.cboNT.FormattingEnabled = true;
+            this.cboNT.Location = new System.Drawing.Point(554, 106);
+            this.cboNT.Name = "cboNT";
+            this.cboNT.Size = new System.Drawing.Size(209, 31);
+            this.cboNT.TabIndex = 103;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Segoe UI", 10.2F);
+            this.label11.Location = new System.Drawing.Point(45, 461);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(62, 23);
+            this.label11.TabIndex = 105;
+            this.label11.Text = "Mô Tả:";
+            // 
+            // ptbAnh
+            // 
+            this.ptbAnh.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ptbAnh.Location = new System.Drawing.Point(195, 461);
+            this.ptbAnh.Name = "ptbAnh";
+            this.ptbAnh.Size = new System.Drawing.Size(554, 285);
+            this.ptbAnh.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbAnh.TabIndex = 106;
+            this.ptbAnh.TabStop = false;
+            this.ptbAnh.Click += new System.EventHandler(this.ptbAnh_Click);
+            // 
+            // frmThemPhong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 846);
+            this.Controls.Add(this.ptbAnh);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cboNT);
+            this.Controls.Add(this.txtSoLuong);
+            this.Controls.Add(this.txtMoTa);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtChieuDai);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtChieuRong);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtDonGia);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cboTT);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.txtTenPT);
+            this.Controls.Add(this.txtMaPT);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Name = "frmThemPhong";
+            this.Load += new System.EventHandler(this.frmThemPhong_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.txtSoLuong)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbAnh)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private FontAwesome.Sharp.IconButton btnThem;
+        private FontAwesome.Sharp.IconButton btnCancel;
+        private System.Windows.Forms.TextBox txtTenPT;
+        private System.Windows.Forms.TextBox txtMaPT;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboTT;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDonGia;
+        private System.Windows.Forms.TextBox txtChieuRong;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtChieuDai;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtMoTa;
+        private System.Windows.Forms.Label label10;
+        private Guna.UI2.WinForms.Guna2NumericUpDown txtSoLuong;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cboNT;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.PictureBox ptbAnh;
+    }
+}
