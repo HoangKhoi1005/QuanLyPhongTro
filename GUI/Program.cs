346847869f063d4b2b4d358cefde6b0991dd99c3
HoangKhoi1005 <158343641+HoangKhoi1005@users.noreply.github.com>
2024-10-08 13:52:31 +0700

    Demo frmDangNhap

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -8,6 +8,7 @@ namespace GUI
 {
     internal static class Program
     {
+        public static frmMain frmMain;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -16,7 +17,8 @@ namespace GUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMain());
+            frmMain = new frmMain();
+            Application.Run(new frmDangNhap());
         }
     }
 }
--- GUI/frmDangNhap.cs
+++ GUI/frmDangNhap.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using SQLServerProvider;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,11 @@ namespace GUI
 {
     public partial class frmDangNhap : Form
     {
+        DBConnect db;
         public frmDangNhap()
         {
             InitializeComponent();
+            db = new DBConnect();
         }
 
         private void label4_Click(object sender, EventArgs e)
@@ -22,6 +26,32 @@ namespace GUI
             frmQuenMatKhau frm = new frmQuenMatKhau();
             frm.Show();
             this.Hide();
+        }
+
+        //Kiểm tra đăng nhập
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            if(txtTaiKhoan.Text == "" || txtMatKhau.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin");
+                return;
+            }
+
+            string query = "SELECT * FROM QUANLY WHERE MAQL = '" + txtTaiKhoan.Text + "' AND MATKHAU = '" + txtMatKhau.Text + "'";
+            using (SqlDataReader reader = db.ExecuteQuery(query))
+            {
+                if (reader.Read())
+                {
+                    MessageBox.Show("Đăng nhập thành công");
+                    frmMain frm = new frmMain();
+                    frm.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Đăng nhập thất bại");
+                }
+            }
         }
     }
 }
