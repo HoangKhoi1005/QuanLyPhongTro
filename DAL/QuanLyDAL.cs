167e814fba20f0085cb3a4da1bdb5eb3a70eae80
vuhuyenvi <153046776+vuhuyenvi@users.noreply.github.com>
2024-10-09 14:59:55 +0700

    chuc nang dang nhap - dung linq

---
+++ DAL/QuanLyDAL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class QuanLyDAL
+    {
+        private QLPTDataContext ql = new QLPTDataContext();
+
+        public bool dangNhap(string maQL, string matKhau)
+        {
+            var query = from quanLy in ql.QUANLies
+                        where quanLy.MAQL == maQL && quanLy.MATKHAU == matKhau
+                        select quanLy;
+
+            if (query.Any())
+            {
+                return true; 
+            }
+            else
+            {
+                return false; 
+            }
+        }
+    }
+}
