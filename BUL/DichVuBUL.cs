9da984808971040f37320c967ce8555c64a85f23
vuhuyenvi <153046776+vuhuyenvi@users.noreply.github.com>
2024-10-17 11:06:04 +0700

    quen mat khau linq

---
+++ BUL/DichVuBUL.cs
@@ -0,0 +1,23 @@
+﻿using DAL;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUL
+{
+    public class DichVuBUL
+    {
+        DichVuDAL DichVuDAL;
+        public DichVuBUL()
+        {
+            DichVuDAL = new DichVuDAL();
+        }
+        public List<DichVuDTO> loadDichVu()
+        {
+            return DichVuDAL.loadDichVu();
+        }
+    }
+}
--- DAL/Properties/Settings.Designer.cs
+++ DAL/Properties/Settings.Designer.cs
@@ -33,5 +33,15 @@ namespace DAL.Properties {
                 return ((string)(this["QL_NhaTroConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=LAPTOP-GQAMABND;Initial Catalog=QL_NhaTro;User ID=sa;Password=123")]
+        public string QL_NhaTroConnectionString1 {
+            get {
+                return ((string)(this["QL_NhaTroConnectionString1"]));
+            }
+        }
     }
 }
